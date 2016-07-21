d6e27e88f35ef1bf2c525de4db9c97025f0e0d5f
Aaron Stricker <aaron.m.stricker@gmail.com>
2016-07-21 19:07:51 +0000

    remove wpa_supplicant from deps

--- deps/wpa_supplicant-2.1/src/common/wpa_ctrl.d
+++
@@ -1,8 +0,0 @@
-../src/common/wpa_ctrl.o: ../src/common/wpa_ctrl.c \
- /home/root/git/edison-sparkfun-oled-control/deps/wpa_supplicant-2.1/src/utils/includes.h \
- /home/root/git/edison-sparkfun-oled-control/deps/wpa_supplicant-2.1/src/utils/build_config.h \
- ../src/common/wpa_ctrl.h \
- /home/root/git/edison-sparkfun-oled-control/deps/wpa_supplicant-2.1/src/utils/common.h \
- /home/root/git/edison-sparkfun-oled-control/deps/wpa_supplicant-2.1/src/utils/os.h \
- /home/root/git/edison-sparkfun-oled-control/deps/wpa_supplicant-2.1/src/utils/wpa_debug.h \
- /home/root/git/edison-sparkfun-oled-control/deps/wpa_supplicant-2.1/src/utils/wpabuf.h
--- makefile
+++ makefile
@@ -1,9 +1,7 @@
 OLED_DIR = deps/Edison_OLED_Block/Firmware/pong
-LIBWPA = deps/wpa_supplicant-2.1/wpa_supplicant/libwpa_ctrl.a
 
 CC = g++
 CFLAGS = -c -Wall -I$(OLED_DIR)/oled -I$(OLED_DIR)/gpio
-CFLAGS += -Ideps/wpa_supplicant-2.1/src/common/
 LDFLAGS =
 SOURCES = src/main.cpp 
 SOURCES += $(OLED_DIR)/spi/spi_port_edison.cpp $(OLED_DIR)/spi/spi_device_edison.cpp 
@@ -14,7 +12,7 @@ EXECUTABLE = main
 all: $(SOURCES) $(EXECUTABLE)
 
 $(EXECUTABLE): $(OBJECTS)
-	$(CC) $(LDFLAGS) $(OBJECTS) $(LIBWPA) -o $@
+	$(CC) $(LDFLAGS) $(OBJECTS) -o $@
 
 .cpp.o:
 	$(CC) $(CFLAGS) $< -o $@
